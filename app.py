a832c32d9860e2b9713fd109b1bb6acaf3f81a7c
EssEnemiGz <biscenp@gmail.com>
2024-06-10 15:46:50 -0400

    Working in settings of database and authentificator.
    The dashboard for califications was created.

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 # LIBRARYS
 from flask import *
-#from flask_cors import CORS
-#import psycopg2
+from dotenv import load_dotenv
+from supabase import *
+import os
 
+# Carga de variables de entorno del .env
+load_dotenv()
+api = os.getenv("SUPABASE_KEY")
+database = os.getenv("SUPABASE_URL")
+secret_key = os.getenv("SECRET_KEY")
+admin_email = os.getenv("EMAIL")
+admin_passw = os.getenv("PASSW")
+
+# Configuracion de la aplicacion web
 app = Flask(__name__)
-#CORS(app)
+if secret_key == None: secret_key = "ewaiourio32i3ujwlelk"
+app.secret_key = secret_key
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+
+# Conexion a base de datos
+db = supabase.create_client(database, api)
+auth_key = supabase.auth.sign_in_with_password( {'email':admin_email, 'password':admin_passw} )
+if auth_key.session:
+    token = auth_key.session.access_token
+    headers = {
+        "Authorization":f"Bearer {token}",
+        "apikey":api
+    }
+else:
+    print(f"Error de auth. {auth_key.error}")
 
 @app.route("/", methods=["GET"])
 def main():
