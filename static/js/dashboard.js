950db4f6921843d6cc976e6171106389e105d14b
EssEnemiGz <biscenp@gmail.com>
2024-06-12 18:43:54 -0400

    "Added new automatization"

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,11 +5,23 @@ fetch("https://softkitacademy-ess123456s-projects.vercel.app/dashboard/calificat
     document.getElementById('username').innerText = data[0];
 
     const main_table = document.getElementById('table');
+    const column = document.getElementById('columns');
     let actual_week;
     for (let j = 1; j < data.length; j++){
         const actual = data[j];
         console.log(j, actual)
-        if (actual_week !== actual.week){ console.log("Cambio de semana")}
+        if (actual_week !== actual.week){ 
+            actual_week = actual.week;
+            console.log("Cambio de semana")
+        }
+
+        if (actual_week === undefined){
+            actual_week = actual.week;
+        }
+
+        const extra_column = document.createElement('h3')
+        extra_column.innerText = actual.task;
+        column.appendChild(extra_column)
 
         const notes = document.createElement('div');
         notes.classList.add('--products-list');
