b46bff0f3ac67358c049978acdabb25f46d29545
EssEnemiGz <biscenp@gmail.com>
2024-06-14 21:12:01 -0400

    "dropdown menu added"

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,22 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// BOTTON TO VIEW THE TOP PAGE
+const dropicon = document.querySelector(".dropicon-block");
+const exiticon = document.querySelector(".exit-icon");
+const links = document.querySelector(".droplinks");
+const menu = document.querySelector(".dropdown");
+
+dropicon.onclick = function() {
+	event.stopPropagation();
+	menu.classList.toggle('open');
+};
+
+exiticon.onclick = function() {
+	event.stopPropagation();
+	menu.classList.toggle('open');
+};
+
+links.onclick = function() {
+	event.stopPropagation();
+	menu.classList.toggle('open');
+}
