7bf8b0e371678205f8b45898884cc5171691b366
EssEnemiGz <biscenp@gmail.com>
2024-09-23 12:07:33 -0400

    "User security system module reworked"

--- microservices/common/user_security.py
+++ microservices/common/user_security.py
@@ -1,17 +1,24 @@
 from time import sleep
 import requests
 import jwt
+import sys
 
 def infinite_retry(func, expected_status, cicle=1):
-    if cicle == 1500: return 500
+    if cicle == 1500: 
+        return 500
     if cicle != 1: sleep(60)
     result = func()
-    print(result)
+    if result == None: 
+        infinite_retry(func, expected_status, cicle+1)
+        
     if expected_status != result.status_code:
         infinite_retry(func, expected_status, cicle+1)
         
-    return result.status_code
+    print(expected_status, result.status_code)
+    if expected_status == result.status_code:
+        sys.exit()
+        return result.status_code
 
 def logged_warning(secret_key, email):
     token = jwt.encode({"data":secret_key}, secret_key, algorithm='HS256')
-    requests.put("https://mail.softkitacademy.com/security/logged", headers={"Content-Type":"application/json", "Accept":"application/json", "Authorization":f"Bearer {token}"}, json={"email":email})
+    requests.put("http://mail.softkitacademy.com/security/logged", headers={"Content-Type":"application/json", "Accept":"application/json", "Authorization":f"Bearer {token}"}, json={"email":email})
